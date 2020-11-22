1f8f50fef4100b010cbce889fc0180eb9d1c25e7
Dhaval K <dhvl.ptl123@yahoo.in>
2020-11-22 12:17:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Play with CI/CD flow
+      </header>
+    </div>
+  );
+}
+
+export default App;
